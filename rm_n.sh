2468f18a7d76e3d46735c2b5c39df0602c046788
ybzhan <126276681+ybzhan@users.noreply.github.com>
2023-03-10 08:08:52 -0600

    Update rm_n.sh

--- rm_n.sh
+++ rm_n.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
-echo "usage: $0  <dir> <n>" 1>&2
+function rm_n {
+  local dir=$1
+  local n=$2
+  echo "usage: $0 <dir> <n>" 1>&2
+  find ./$dir -size +$n'c'
+  find ./$dir -type f -size +$n'c' -exec rm -f {} \;
+}
 
-dir=$1
-n=$2
-
-
-find /$dir -type f -size +$n'c' -exec rm -f {} \;
+rm_n five 3
+#echo rm_n five 3
